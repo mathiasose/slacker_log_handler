f6a33b78ddba2efef54670672d1b35aa1b1edcac
Mathias Ose <mathias.ose@gmail.com>
2015-10-22 21:32:17 +0200

    v1.0.2 version bump download_url too

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import setup
 setup(
     name='slacker_log_handler',
     packages=['slacker_log_handler'],
-    version='1.0.1',
+    version='1.0.2',
     description='Posts log events to Slack via API',
     url='https://github.com/mathiasose/slacker_log_handler',
-    download_url='https://github.com/mathiasose/slacker_log_handler/tarball/1.0',
+    download_url='https://github.com/mathiasose/slacker_log_handler/tarball/1.0.2',
     author='Mathias Ose',
     author_email='mathias.ose@gmail.com',
     keywords=['slack', 'logging'],

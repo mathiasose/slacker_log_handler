4ed8a327a3cc687b7f100077514c52cf46ae3a30
Leonardo Gatica <lgatica@protonmail.com>
2015-10-23 14:36:09 -0300

    Add extra info to setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,43 @@
 # -*- coding: utf-8 -*-
+import os
+
 from setuptools import setup
+
+
+def readme(*paths):
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
 
 setup(
     name='slacker_log_handler',
     packages=['slacker_log_handler'],
+    py_modules=['slacker_log_handler'],
     version='1.0.2',
     description='Posts log events to Slack via API',
+    long_description=readme('README.rst'),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Communications :: Chat',
+        'Topic :: Office/Business :: Groupware',
+    ],
     url='https://github.com/mathiasose/slacker_log_handler',
     download_url='https://github.com/mathiasose/slacker_log_handler/tarball/1.0.2',
     author='Mathias Ose',
     author_email='mathias.ose@gmail.com',
     keywords=['slack', 'logging'],
     install_requires=[
-        "slacker==0.7.3"
-    ]
+        'slacker==0.7.3'
+    ],
+    include_package_data=True,
+    zip_safe=False
 )

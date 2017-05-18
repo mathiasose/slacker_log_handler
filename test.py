0ca830b6e0e4aa25325dd8ef5d64aef404a45c4a
Mathias Ose <mathias.ose@gmail.com>
2017-05-18 22:54:35 +0200

    Add some basic testing

---
+++ test.py
@@ -0,0 +1,31 @@
+import logging
+import os
+from platform import python_version
+
+from slacker_log_handler import SlackerLogHandler
+
+SLACK_API_TOKEN = os.getenv('SLACK_API_TOKEN')
+SLACK_CHANNEL = os.getenv('SLACK_CHANNEL')
+
+slack_handler = SlackerLogHandler(SLACK_API_TOKEN, SLACK_CHANNEL)
+
+logger = logging.getLogger('debug_application')
+logger.addHandler(slack_handler)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+slack_handler.setFormatter(formatter)
+
+logger.info('Python version is {}'.format(python_version()))
+
+logger.debug('Test DEBUG')
+logger.info('Test INFO')
+logger.warning('Test WARNING')
+logger.error('Test ERROR')
+logger.fatal('Test FATAL')
+logger.critical('Test CRITICAL')
+
+try:
+    raise Exception('Test exception')
+except Exception as e:
+    logger.exception(e)
